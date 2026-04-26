fc8fd26b463412d892af498a6c0398b6bf342855
Methre Amarnath <methreamarnath@gmail.com>
2026-04-26 11:29:09 +0530

    updated the app.js and server,js to work with the endpoins

--- backend/server.js
+++ backend/server.js
@@ -5,4 +5,9 @@ connectToDB();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
+  console.log("create a poll at http://localhost:3000/polls");
+  console.log("get all polls at http://localhost:3000/polls");
+  console.log("get poll results at http://localhost:3000/polls/:id/results");
+  console.log("vote on a poll at http://localhost:3000/polls/:id/vote");
+  console.log("delete a poll at http://localhost:3000/polls/:id");
 });
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
-
+const pollRoutes = require("./routes/pollroutes");
 const app = express();
 
 app.use(express.json());
+app.use("/polls", pollRoutes);
 
 app.get("/health", (req, res) => {
   res.send("the server is running");
