c3476309eab8a63797f813aebfe63f2d4dc8e12e
Methre Amarnath <methreamarnath@gmail.com>
2026-04-26 11:34:27 +0530

    added the middlewares and updated in the server with cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,19 @@
 const express = require("express");
-const pollRoutes = require("./routes/pollroutes");
+const cors = require("cors");
+const { notFound, errorHandler } = require("./middlewares/errormiddleware.js");
 const app = express();
-
+const pollRoutes = require("./routes/pollroutes");
+// Middleware
+app.use(cors());
 app.use(express.json());
+
+// Routes
 app.use("/polls", pollRoutes);
 
+// Error handling
+app.use(notFound);
+app.use(errorHandler);
+// Health check endpoint
 app.get("/health", (req, res) => {
   res.send("the server is running");
 });
---
+++ backend/src/middlewares/errormiddleware.js
@@ -0,0 +1,13 @@
+const notFound = (req, res, next) => {
+  res.status(404).json({ message: "Route not found" });
+};
+
+const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal Server Error" });
+};
+
+module.exports = {
+  notFound,
+  errorHandler,
+};
